b2008b99a837fa02018478725f0ee40541000e8f
Julio Martin <faria@DESKTOP-TLUUL68>
2024-09-04 19:06:23 -0300

    Modificando el archivo test

--- Programación/test.py
+++ Programación/test.py
@@ -5,17 +5,7 @@ import time
 almacen = Stock();
 print(almacen.get_violines_electricos());
 
-""" violin = ViolinElectico(marca = "Yamaha",
-    modelo = "SV-200",
-    numero_serie="123456789",
-    color = "Negro",
-    tipo_puente = "Regulable",
-    tipo_cuerdas = "Sintéticas",
-    amplificador_incluido = True,
-    nivel_bateria = 0
-    );
-print(f"Los datos {violin}");
-violin.prender(); """
+
 """ print(f"Horario de encendido {violin.get_horario_encendido()}");
 time.sleep(25);
 violin.apagar();
