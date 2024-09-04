e0ff5b5576c36cda0bd5921fb793933b48a1f446
Julio Martin <faria@DESKTOP-TLUUL68>
2024-09-04 11:51:51 -0300

    Creando el metodo tiempo del encendido

--- Programación/violinElectronico.py
+++ Programación/violinElectronico.py
@@ -1,3 +1,4 @@
+import datetime
 class ViolinElectico:
     def __init__(self, marca, modelo, numero_serie, color, tipo_puente, tipo_cuerdas, amplificador_incluido, nivel_bateria ):
         self.__marca = marca;
@@ -8,6 +9,7 @@ class ViolinElectico:
         self.__tipo_cuerdas = tipo_cuerdas;
         self.__amplificador_incluido = amplificador_incluido;
         self.__nivel_bateria = nivel_bateria;
+        self.__horario_encendido = 0;
        
     def get_marca(self):
            return self.__marca;
@@ -34,7 +36,7 @@ class ViolinElectico:
         self.__color = color;
 
     def get_tipo_puente(self):
-        return self.__tipo_puente
+        return self.__tipo_puente;
     
     def set_tipo_puente(self, tipo_puente):
         self.__tipo_puente = tipo_puente;
@@ -52,11 +54,27 @@ class ViolinElectico:
         self.__amplificador_incluido = amplificador_incluido;
     
     def get_nivel_bateria(self):
-        return self.__nivel_bateria
+        return self.__nivel_bateria;
     
     def set_nivel_bateria(self, value):
-        self.__nivel_bateria = value
+        self.__nivel_bateria = value;
+    
+    def get_horario_encendido(self):
+        return self.__horario_encendido
+    
+    def set_horario_encendido(self, horario_encendido):
+        self.__horario_encendido = horario_encendido;
+
  
+    def apagar(self):
+        return;
+
+    def prender(self):
+        self.set_horario_encendido(datetime.datetime.now());
+        return;
+
+    def cargar_bateria(self):
+        return
 
     def __str__(self):
         return (f"ViolinElectrico(marca={self.__marca}, modelo={self.__modelo}, "
